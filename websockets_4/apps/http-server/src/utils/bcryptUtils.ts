1aff27a7b0e4bebbb4c963d0699049b3a3ef137b
Junaid Nawaz <shaikhmohammedjunaidnawaz@gmail.com>
2026-09-25 03:57:09 +0530

    (apps): add bcrypt and jwt utils

--- websockets_4/apps/http-server/src/utils/bcryptUtils.ts
+++ websockets_4/apps/http-server/src/utils/bcryptUtils.ts
@@ -4,3 +4,11 @@ export const createHash = async (password: string): Promise<string> => {
   const hashedPassword = await bcrypt.hash(password, 10);
   return hashedPassword;
 };
+
+export const verifyHash = async (
+  password: string,
+  hashedPassword: string,
+): Promise<boolean> => {
+  const isVerified = await bcrypt.compare(password, hashedPassword);
+  return isVerified;
+};
---
+++ websockets_4/apps/http-server/src/utils/jwtUtils.ts
@@ -0,0 +1,46 @@
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+export interface JwtData {
+  id?: string | undefined;
+  email?: string | undefined;
+  message?: string | undefined;
+}
+export enum TOKEN {
+  ACCESS_TOKEN,
+  REFRESH_TOKEN,
+}
+
+export const access_token_options = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV == "production" ? true : false,
+  sameSite: "lax",
+  maxAge: 1000 * 60 * 15,
+};
+
+export const refresh_token_options = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV == "production" ? true : false,
+  sameSite: "lax",
+  maxAge: 1000 * 60 * 60 * 24 * 7,
+};
+
+// create a jwt token
+export const createToken = (user: JwtData, token: TOKEN) => {
+  if (token == TOKEN.ACCESS_TOKEN) {
+    // access token logic
+    const access_token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_ACCESS_SECRET as string,
+      { expiresIn: "15m" },
+    );
+    return access_token;
+  } else if (token == TOKEN.REFRESH_TOKEN) {
+    // refresh token logic
+    const refresh_token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_REFRESH_SECRET as string,
+      { expiresIn: "7d" },
+    );
+    return refresh_token;
+  }
+};
